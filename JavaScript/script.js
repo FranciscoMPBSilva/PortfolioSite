3b9c5110f7e8eefccde3ca78c26d4c69e9980115
FranciscoMPBSilva <franciscompbsilva2016@gmail.com>
2025-07-08 14:29:32 +0100

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let currentSectionId = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.offsetHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      currentSectionId = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + currentSectionId) {
+      link.classList.add("active");
+    }
+  });
+});
+
+document.querySelector('.aboutmeButton').addEventListener('click', () => {
+  document.querySelector('#aboutme').scrollIntoView({ behavior: 'smooth' });
+});
+
+function updateScrollPadding() {
+  const navbar = document.querySelector(".navbar");
+  const height = navbar.offsetHeight;
+  document.documentElement.style.setProperty("--scroll-padding", height + "px");
+}
+
+window.addEventListener('load', updateScrollPadding);
+window.addEventListener('resize', updateScrollPadding);
+
+
+
+
